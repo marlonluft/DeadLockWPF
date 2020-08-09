188f8110df1020c8df3006837ded84bc31fce134
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2020-08-09 19:51:02 -0300

    Custom exception for logic

---
+++ Wpf_DeadLock/DeadLock.Logic/Exceptions/LogicException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DeadLock.Logic.Exceptions
+{
+    public class LogicException : Exception
+    {
+        public LogicException()
+        {
+        }
+
+        public LogicException(string message) : base(message)
+        {
+        }
+
+        public LogicException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected LogicException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
